695304580a0c2936c3b71cd3c8332a74734b7a51
Calphonar <Richardzejak@hotmail.com>
2020-08-27 15:33:45 +0200

    Added writelines and readlines for user to choose operators and terms and put them into variables

--- Inlamningsuppgift-1/Program.cs
+++ Inlamningsuppgift-1/Program.cs
@@ -8,5 +8,17 @@ class Program
         int term1, term2, term3;
         string operator1;
         string operator2;
+
+        Console.WriteLine("Enter first operator: ");
+        operator1 = Console.ReadLine();
+        Console.WriteLine("Enter second operator: ");
+        operator2 = Console.ReadLine();
+        Console.WriteLine("Enter first term: ");
+        term1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second term: ");
+        term2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter third term: ");
+        term3 = Convert.ToInt32(Console.ReadLine());
+
     }
 }
