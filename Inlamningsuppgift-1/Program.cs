4b80b21372ff9abddf21457a1871d5bdd98df713
Richard Zejak <Richardzejak@hotmail.com>
2020-09-02 15:31:02 +0200

    added input checks to operators, also cleared up some code and added comments

--- Inlamningsuppgift-1/Program.cs
+++ Inlamningsuppgift-1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
-
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
 
 class Program
 {
@@ -7,20 +9,46 @@ class Program
     {
 
         int term1, term2, term3;
-        string operator1;
-        string operator2;
+        bool validInput = false;
+        string operator1 = "o";
+        string operator2 = "o";
 
-        Console.WriteLine("Enter first operator: ");
-        operator1 = Console.ReadLine();
-        Console.WriteLine("Enter second operator: ");
-        operator2 = Console.ReadLine();
-        Console.WriteLine("Enter first term: ");
+        Console.WriteLine("Enter the first term: "); // asks the user for the first term and sets term1 variable
         term1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter second term: ");
-        term2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter third term: ");
-        term3 = Convert.ToInt32(Console.ReadLine());
 
+        while (!validInput) // asks the user to write the first operator, and also checks if the input is valid
+        {
+            Console.WriteLine("Enter first operator: ");
+            operator1 = Console.ReadLine();
+            if (operator1 == "+" || operator1 == "-" || operator1 == "*" || operator1 == "/")
+                validInput = true;
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                Console.Beep();
+            }
+        }
+
+        Console.WriteLine("Enter the second term: "); // asks the user for the second term and sets term2 variable
+        term2 = Convert.ToInt32(Console.ReadLine());
+
+        validInput = false; // sets validinput variable to false so it will loop when asking user to input the second operator
+
+        while (!validInput) // asks the user to write the second operator, and also checks if the input is valid
+        {
+            Console.WriteLine("Enter the second operator: ");
+            operator2 = Console.ReadLine();
+            if (operator2 == "+" || operator2 == "-" || operator2 == "*" || operator2 == "/")
+                validInput = true;
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                Console.Beep();
+            }
+        }
+        Console.WriteLine("Enter third term: "); // asks the user for the third term and sets term3 variable
+        term3 = Convert.ToInt32(Console.ReadLine());
+        // checks the operators and then prints out the whole equation depending on operators and terms
         if (operator1.Equals("+") && operator2.Equals("+"))
             Console.WriteLine("{0} + {1} + {2} = {3}", term1, term2, term3, term1 + term2 + term3);
         if (operator1.Equals("+") && operator2.Equals("-"))
@@ -56,6 +84,6 @@ class Program
             Console.WriteLine("{0} / {1} * {2} = {3}", term1, term2, term3, term1 / term2 * term3);
         if (operator1.Equals("/") && operator2.Equals("/"))
             Console.WriteLine("{0} / {1} / {2} = {3}", term1, term2, term3, term1 / term2 / term3);
-        Console.Read();
+        Console.Read(); // only there for a clean ending
     }
 }
